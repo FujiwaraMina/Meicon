f83cd9271f46bd7104b3fb77fda05c5732109674
Your Name <you@example.com>
2022-11-05 09:38:58 +0000

    [Addコントローラ、ルーティング]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users, only:[:show,:edit]
+    resources :posts, only:[:show]
+    root to: "homes#top"
+  end
+
+  namespace :public do
+    root to: "homes#top"
+    get 'homes/about'
+
+    resources :posts, only:[:index,:show,:create,:edit,:destroy,:update] do
+      resources :comments, only:[:create] #コメント
+      resource :favorites, only:[:create,:destroy] #いいね
+    end
+
+    resources :users, only:[:index,:show,:edit] do
+      resource :relationships, only:[:create,:destroy]
+      get 'followings' => 'relationships#followings', as: 'followings' #フォロー
+      get 'followers'  => 'relationships#followers' , as: 'followers' #フォロワー
+    end
+  end
+
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
