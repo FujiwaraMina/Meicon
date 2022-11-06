db9941028abf82f341d2cadc629f19068c1b1b26
Your Name <you@example.com>
2022-11-06 10:19:46 +0000

    [Add]ログイン、新規登録、ゲストログイン

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts.clear
+  # config.hosts.clear
+  config.web_console.whitelisted_ips = '121.82.16.156'
+  config.hosts << "6ca2dd740eb54946a84dc5d71b6aea6d.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,37 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations:  "public/registrations",
+    sessions:       "public/sessions"
+  }
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   namespace :admin do
-    resources :users, only:[:show,:edit]
+    resources :users, only:[:show,:edit,:update]
     resources :posts, only:[:show]
     root to: "homes#top"
   end
 
-  namespace :public do
+  scope module: :public do
     root to: "homes#top"
-    get 'homes/about'
+    get 'about' => 'homes#about'
 
     resources :posts, only:[:index,:show,:create,:edit,:destroy,:update] do
       resources :comments, only:[:create] #コメント
       resource :favorites, only:[:create,:destroy] #いいね
     end
 
-    resources :users, only:[:index,:show,:edit] do
+    resources :users, only:[:index,:show,:edit,:update] do
       resource :relationships, only:[:create,:destroy]
       get 'followings' => 'relationships#followings', as: 'followings' #フォロー
       get 'followers'  => 'relationships#followers' , as: 'followers' #フォロワー
     end
   end
-
-  devise_for :admins
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
