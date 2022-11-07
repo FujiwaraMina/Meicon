b03b804d87490d3d4dfb4eb1c263b300bac0e6bf
Your Name <you@example.com>
2022-11-07 06:18:44 +0000

    [Add]会員一覧、詳細

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,12 @@ class User < ApplicationRecord
       user.name = "guestuser"
     end
   end
+  #ユーザー画像定義付け
+  def get_user_image(width,height)
+    unless user_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      user_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
+    end
+    user_image.variant(resize_to_limit:[width,height]).processed
+  end
 end
