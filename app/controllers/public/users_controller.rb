903dc7ec4a5b0a9da854f8f4872c4560aa7bb62a
Your Name <you@example.com>
2022-11-07 12:39:31 +0000

    [Add]会員編集

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,5 +9,19 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :user_image, :user_caption, :email)
   end
 end
