95f40c6abac5de7c9c028a59da4db457617e09fb
Your Name <you@example.com>
2022-11-25 04:05:26 +0000

    [Fix]フォロー非同期

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,11 +2,13 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
   #フォローする時
   def create
+    @users = User.all
     @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
   end
   #フォロー外す時
   def destroy
+    @users = User.all
     @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
   end
